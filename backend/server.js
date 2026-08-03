aad5a026c7742b528730e57cfbf9beb84b6b07a6
ANTONIO  NETO <snetinho40@gmail.com>
2026-08-03 19:19:47 -0300

    up

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,24 @@
 const express = require("express");
 const app = express();
 
+const produtos = [
+    {
+        id: 1,
+        nome: "Shampoo hidratante",
+        preco: 49
+    },
+    {
+        id: 2,
+        nome: "Creme anti-idade",
+        preco: 89
+    }
+];
 app.get("/", (req, res) => {
     res.send("Olá, mundo!");
+});
+
+app.get("/produtos", (req, res) => {
+    res.json(produtos);
 });
 
 app.listen(3000, () => {
