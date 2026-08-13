5d9627e569352afc140ce7036fd251d0833fd471
ANTONIO  NETO <snetinho40@gmail.com>
2026-08-13 20:20:48 -0300

    up

--- links/produtos.js
+++ links/produtos.js
@@ -1,7 +1,39 @@
 fetch("http://localhost:3000/produtos")
     .then(resposta => resposta.json())
     .then(produtos => {
-        console.log("Produtos recebidos:", produtos);
+
+        const listaProdutos = document.getElementById("lista-produtos");
+        const listaNovidades = document.getElementById("lista-novidades");
+
+        produtos.forEach(produto => {
+
+            const div = document.createElement("div");
+
+            if (produto.secao === "destaque") {
+                div.classList.add("produto");
+            }
+
+            if (produto.secao === "novidade") {
+                div.classList.add("novidade");
+            }
+
+            div.innerHTML = `
+                <img src="${produto.imagem}" alt="${produto.nome}">
+                <h3>${produto.nome}</h3>
+                <p>R$ ${produto.preco}</p>
+                <a href="${produto.link}" class="btn">Ver Detalhes</a>
+            `;
+
+            if (produto.secao === "destaque") {
+                listaProdutos.appendChild(div);
+            }
+
+            if (produto.secao === "novidade") {
+                listaNovidades.appendChild(div);
+            }
+
+        });
+
     })
     .catch(erro => {
         console.error("Erro ao buscar produtos:", erro);
