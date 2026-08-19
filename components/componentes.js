854b955192d5e0d37339f6cba9266cd38b5a546f
ANTONIO  NETO <snetinho40@gmail.com>
2026-08-19 16:46:16 -0300

    up

--- components/componentes.js
+++ components/componentes.js
@@ -1,4 +1,4 @@
-
+// HEADER
 const caminhoComponentes = new URL(".", import.meta.url);
 
 fetch(new URL("header.html", caminhoComponentes))
@@ -26,3 +26,22 @@ const css = document.createElement("link");
 css.rel = "stylesheet";
 css.href = new URL("header.css", caminhoComponentes);
 document.head.appendChild(css);
+
+// FOOTER
+fetch(new URL("footer.html", caminhoComponentes))
+    .then(resposta => resposta.text())
+    .then(footer => {
+        const elementoFooter = document.getElementById("footer");
+
+        if (elementoFooter) {
+            elementoFooter.innerHTML = footer;
+        }
+    })
+    .catch(erro => {
+        console.error("Erro ao carregar o footer:", erro);
+    });
+
+const cssFooter = document.createElement("link");
+cssFooter.rel = "stylesheet";
+cssFooter.href = new URL("footer.css", caminhoComponentes);
+document.head.appendChild(cssFooter);
