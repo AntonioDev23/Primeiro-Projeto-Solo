98db1ca4d824c0ca7941afa6e52627d1d38422dd
ANTONIO  NETO <snetinho40@gmail.com>
2026-09-02 18:45:56 -0300

    up

--- links/produtos.js
+++ links/produtos.js
@@ -1,44 +1,75 @@
 fetch("http://localhost:3000/produtos")
     .then(resposta => {
+
         if (!resposta.ok) {
             throw new Error("Erro ao buscar produtos");
         }
 
         return resposta.json();
+
     })
     .then(produtos => {
 
-        const listaProdutos = document.getElementById("lista-produtos");
-        const listaNovidades = document.getElementById("lista-novidades");
+        const listaProdutos =
+            document.getElementById("lista-produtos");
+
+        const listaNovidades =
+            document.getElementById("lista-novidades");
+
 
         produtos.forEach(produto => {
 
             const div = document.createElement("div");
 
-            div.classList.add(
-                produto.secao === "destaque"
-                    ? "produto"
-                    : "novidade"
-            );
+            div.classList.add("card-produto");
+
 
             div.innerHTML = `
-                <img src="${produto.imagem}" alt="${produto.nome}">
-                <h3>${produto.nome}</h3>
-                <p>R$ ${Number(produto.preco).toFixed(2)}</p>
-                <a href="${produto.link}" class="btn">Ver Detalhes</a>
+
+                <img
+                    src="${produto.imagem}"
+                    alt="${produto.nome}"
+                >
+
+                <h3>
+                    ${produto.nome}
+                </h3>
+
+                <p>
+                    R$ ${Number(produto.preco).toFixed(2)}
+                </p>
+
+                <a
+                    href="${produto.link}"
+                    class="btn"
+                >
+                    Ver Detalhes
+                </a>
+
             `;
 
+
             if (produto.secao === "destaque") {
+
                 listaProdutos.appendChild(div);
+
             }
 
+
             if (produto.secao === "novidade") {
+
                 listaNovidades.appendChild(div);
+
             }
 
         });
 
     })
     .catch(erro => {
-        console.error("Erro ao buscar produtos:", erro);
+
+        console.error(
+            "Erro ao buscar produtos:",
+            erro
+        );
+
     });
